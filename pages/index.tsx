dffb5884e51708b2189eea0739ea79e5eafecd41
Cheri Tsukagoshi <akiwidev@gmail.com>
2023-02-21 21:53:16 +0900

    Added prettier to project

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
 const HomePage = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold p-4">Weekly List</h1>
-
+    <div className="h-screen border-2 border-red-400">
+      <div className="flex align-middle justify-center border-2 border-purple-500">
+        <h1 className="text-3xl font-bold p-4">Create Itinerary</h1>
+      </div>
     </div>
   );
 };
